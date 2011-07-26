1b8a8bbd4025786b2e175e8a588d11598c2297cb
ian asaff <ian.asaff@gmail.com>
2011-07-26 12:57:08 -0400

    removed legacy instance variables

--- lib/sell_knives/deck.rb
+++ lib/sell_knives/deck.rb
@@ -1,13 +1,13 @@
 module SellKnives
   class Deck
     def self.randomized
-      @cards = []
+      cards = []
       %w{A 2 3 4 5 6 7 8 9 10 J Q K}.each do |value|
         %W{C S D H}.each do |suit|
-          @cards << Card.new(value,suit)
+          cards << Card.new(value,suit)
         end
       end
-      @cards.shuffle!
+      cards.shuffle
     end
   end
 end
